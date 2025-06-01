d97d4203256dd45d75e7292f313211a44755bffa
Andres Cordero <an.cornmo@gmail.com>
2025-05-31 18:42:29 -0600

    Keyboard function added

--- BikeSpeedometer.py
+++ BikeSpeedometer.py
@@ -1,8 +1,17 @@
 from pynput import keyboard
 import time
 
-with keyboard.Listener() as listener:
-    print("Presioná SPACE para simular vueltas de rueda. Ctrl+C para salir.")
+def on_press(key):
+    if key == keyboard.Key.space:
+        print("¡Se presionó SPACE!")
+    elif key == keyboard.Key.esc:
+        print("Saliendo...")
+        # Detener el listener para salir del programa
+        return False
+
+
+
+with keyboard.Listener(on_press=on_press) as listener:
+    print("Presiona SPACE para ver un mensaje, ESC para salir.")
     listener.join()
 
-    
